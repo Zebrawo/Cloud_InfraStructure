48742190bf3a9a29e4b3775b0ae153d4bf408701
Timmy <78703970+Zebrawo@users.noreply.github.com>
2023-04-04 18:39:37 +0200

    Create mongo_functions.py
    MongoDB functions voor het toevoegen van data

---
+++ webgui/mongo_functions.py
@@ -0,0 +1,68 @@
+from pymongo import MongoClient
+from datetime import datetime
+from pprint import pprint
+
+# Connect to MongoDB
+client = MongoClient()
+db = client.cli
+mongo_users = db.users
+mongo_records = db.Arecords
+
+def add_user(name, email, token):
+    user = mongo_users.find_one({'email': email})
+    if user:
+        print("User already excists")
+        return user['_id']
+    else:
+        user = {
+            'name': name,
+            'email': email,
+            'token': token,
+            'codes': []
+        }
+        result = mongo_users.insert_one(user)
+        print(f"Customer with ID {result.inserted_id} created.")
+        return result.inserted_id
+
+
+def time_change_FQDN(fqdn, ipv4,email):
+    # Check if the FQDN already exists in the database
+    record = mongo_records.find_one({"fqdn": fqdn})
+    if record:
+        # If the record already exists, check if the IP address has changed
+        if record["ipv4"] != ipv4:
+            # If the IP address has changed, update the record with the new IP address and timestamp
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            result = mongo_records.update_one({"fqdn": fqdn}, {"$set": {"ipv4": ipv4, "timestamp": dt_string, "action": "changed"}})
+            print(f"Record with FQDN {fqdn} updated. New IP address: {ipv4}")
+        else:
+            print(f"Record with FQDN {fqdn} already exists.")
+    else:
+        # If the record doesn't exist, add it to the database with the current timestamp
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        record = {
+            "fqdn": fqdn,
+            "ipv4": ipv4,
+            "email": email,
+            "action": "created",
+            "timestamp": dt_string
+        }
+        result = mongo_records.insert_one(record)
+        print(f"Record with FQDN {fqdn} and IP address {ipv4} created.")
+
+        return result.inserted_id
+    
+def change_action_delete(fqdn, email):
+    # Zoek het document met de gegeven FQDN
+    record = mongo_records.find_one({"fqdn": fqdn})
+    if record:
+        # Update het document met de nieuwe actie en timestamp
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        result = mongo_records.update_one({"fqdn": fqdn}, {"$set": {"timestamp": dt_string, "action": "deleted", "email": email}})
+        print(f"Record with FQDN {fqdn} updated with action deleted and timestamp {dt_string}.")
+        return result
+    else:
+        print(f"No record found with FQDN {fqdn}.")
