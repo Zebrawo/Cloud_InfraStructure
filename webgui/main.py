1db99cebe51dfe4f88fe5181649d602c8a57d76b
Timmy <78703970+Zebrawo@users.noreply.github.com>
2023-04-04 19:27:16 +0200

    Update main.py

--- webgui/main.py
+++ webgui/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, make_response, session, flash
 from authomatic.adapters import WerkzeugAdapter
 from authomatic import Authomatic
 import authomatic
-import logging
 from dnszone import *
 from mongo_functions import *
 from config import CONFIG
@@ -22,18 +21,20 @@ def index():
 
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
 def login(provider_name):
-    # We need response object for the WerkzeugAdapter.
+    # We hebben een response object nodig voor Werkzeugner
     response = make_response()
-    # Log the user in, pass it the adapter and the provider name.
+    # Log de gebruiker in, geef hem de adapter en de providernaam door.
     result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
 
-    # If there is no LoginResult object, the login procedure is still pending.
+    # Als er geen LoginResult-object is, is de aanmeldingsprocedure nog in behandeling.
     if result:
         if result.user:
-            # We need to update the user to get more info.
+            # We moeten de gebruiker update voor meer informatie
             result.user.update()
+            # Voeg de gebruiker toe aan de MongoDB
             add_user(result.user.name,result.user.email,result.user.id)
-
+            
+            #Zet gegevens vast in Session zodat we deze herkunnen gebruiken
             session['email'] = result.user.email
             session['result'] = {
             'provider_name': result.provider.name,
@@ -43,51 +44,63 @@ def login(provider_name):
             'error': None
         }
 
-        # The rest happens inside the template.
+        # De rest gebeurd in de template
         return render_template('login.html', result=result)
 
-    # Don't forget to return the response.
+    # Vergeet niet het antwoord terug te sturen
     return response
 
+#Route voor het toevoegen van een A Record
 @app.route('/login/google/add_a_record/', methods=['post'])
 def add_a_record():
-    #via post krijg je de naam van de machine binnen.
-    input_fqdn = request.form['fqdn']
-    #Hier wordt de FQDN gemaakt
-    dnsname = input_fqdn + ".cli.test"
-    #Via post krijg je de IP address binnen
+    # variable maken met input van de form van de website met behulp van post
+    dnsname = request.form['fqdn'] + ".cli.test"
     ip = request.form['ipv4']
-    #Toevoegen van de A record met de bijhorende gegevens
+    # Toevoegen van de A record met de bijhorende gegevens
     dnsmang.add_address(fqdn=dnsname,ipv4=ip)
-    #Toevoegen door wie de wijziging is gebeurd aan MongoDB.
+    # Toevoegen door wie de wijziging is gebeurd aan MongoDB.
     email = session.get('email')
     time_change_FQDN(dnsname, ip, email)
-    #Sessie binnenhalen
+    # Sessie binnenhalen
     result = session.get('result')
+    # Weergeven op de website dat de A record is toegevoegd
     flash(f"record voor {dnsname} met IP-adres {ip} is toegevoegd")
-    
+
     return render_template('login.html', result=result)
 
+# Route voor het aanpassen van de A Record
 @app.route('/login/google/edit_a_record/', methods=['post'])
 def edit_a_record():
+    # variable maken met input van de form van de website met behulp van post
     dnsname = request.form['fqdn'] + ".cli.test"
     ip = request.form['ipv4']
+    # Het aanpassen van de A record met de bijbehorende gegevens
     dnsmang.update_address(fqdn=dnsname,ipv4=ip)
+    # Sessie data binnenhalen zodat we de pagina statisch kunnen houden
     email = session.get('email')
+    # Opslaan van de wijziging in MongoDB
     time_change_FQDN(dnsname, ip, email)
-    #Sessie binnenhalen
+    # Sessie binnenhalen
     result = session.get('result')
+    # Weergeven op de website dat de A record is aangepast
+    flash(f"record voor {dnsname} met IP-adres {ip} is aangepast")
 
     return render_template('login.html', result=result)
 
+# Route voor het verwijderen van een A Record
 @app.route('/login/google/delete_a_record/', methods=['post'])
 def delete_a_record():
+    # Variable aanmaken met input vanuit de website met behulp van post
     dnsname = request.form['fqdn'] + ".cli.test"
     dnsmang.clear_address(fqdn=dnsname)
+    # Sessie data binnen halen
     email = session.get('email')
+    # Wijziging opslaan in database
     change_action_delete(dnsname, email)
-    #Sessie binnenhalen
+    # Sessie binnenhalen
     result = session.get('result')
+    # Weergeven op de website dat de A record is verwijderd
+    flash(f"record voor {dnsname} is verwijderd")
 
     return render_template('login.html', result=result)
 
