4f5b40c6b966c8dea908d30b2a9bb28f35d38d43
Timmy <78703970+Zebrawo@users.noreply.github.com>
2023-04-12 23:59:08 +0200

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -36,7 +36,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     admin_username = "azureuser"
 
     ssh_key {
-      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVf0nttI/P2OyNYcAWXDErhSwupd9o2fNin2gOL8R7YBz3se1HCsXMYa7scZyFBzH3XGgaUAXIc0AvHXllUaws2aLZLycB50+7W0yii6P0bUTMZmCNeFx2h6qIkmDc09NMlNdHwtD9rSMElV8gdU/66Um83okJKqu5XQZufxrZ0vw2ea98XUxURojkivWetYoEBPaYHjHlZjxyEFcRk4x8E0esDmhwO4S37cHlv7fGq9y5e3fsWytbQMEiNNx5/hKbYqJxmWXrGMGBUuNshOuyalGrJ04T3ePGhzX+eYoPIue17IQScSPiV35gsdZP1fJvhedpIR41PMY8DB5SNXnN rsa-key-20230411"
+      key_data = "<your public SSH KEY>"
     }
   }
 
